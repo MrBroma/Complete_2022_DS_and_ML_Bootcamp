b48186a29d82c262481f90d5a7740df0be1b556c
Loic Rouaud <loic.rouaud@gmail.com>
2022-06-17 00:35:00 +0200

    Section 2 lesson and exercises

---
+++ Section_3/life.py
@@ -0,0 +1,12 @@
+import math
+
+
+theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated yout chance of survival, but I don\'t think you\'ll like it')
+
+
+def square_root(x):
+    return math.sqrt(x)
+
